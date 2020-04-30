8fa9fba2fbf930eb212adfdad3e3812a76513cfd
Silas P. Ladislau <splhead@gmail.com>
2020-04-29 21:11:23 -0400

    start frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/upload';
-import cors from 'cors';
 
 import './database';
 import AppError from './errors/AppError';
